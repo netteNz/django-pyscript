0c8472358626d3e49fe0a9863ed437ce5c43b84c
Emanuel Lugo <lugo.emanuel@gmail.com>
2024-08-09 12:13:33 -0400

    styling

--- landing/urls.py
+++ landing/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.landing_page, name='landing_page'),
+    path('about/', views.about_page, name='about_page'),
 ]
--- landing/views.py
+++ landing/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def landing_page(request):
     return render(request, 'landing/landing.html')
+
+def about_page(request):
+    return render(request, 'landing/about.html')
